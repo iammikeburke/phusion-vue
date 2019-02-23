770afab294b4629a37804b0de7cb89c3f603042d
Mike Burke <michael.burke@wzukltd.com>
2019-02-23 14:37:09 +0000

    [webpack]: Updated webpack config to bundle routes task and templates task

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,8 @@ var env = process.env.ENV ? process.env.ENV : 'dev';
  * Config Options
  */
 var classmapTaskPath = './exports/build/ClassmapTask';
+var templatesTaskPath = './exports/build/TemplatesTask';
+var routesTaskPath = './exports/build/RoutesTask';
 var outputFileExt = (env === 'prod') ? '.min.js' : '.js';
 var sourceMapOutputFileExt = '.map';
 var mode = (env === 'prod') ? 'production' : 'development';
@@ -40,6 +42,90 @@ module.exports = [
       extensions: ['.js', '.ts']
     },
     target:       "node",
+    module:       {
+      rules: [
+        {
+          test:    /\.ts/,
+          exclude: /node_modules\/(?!phusion|vue).*/,
+          use:     [
+            {
+              loader:  'ts-loader',
+              options: {
+                context: __dirname,
+              },
+            }
+          ]
+        }
+      ]
+    }
+  },
+  /**
+   * TemplateCacheTask
+   */
+  {
+    mode:         mode,
+    watch:        false,
+    watchOptions: {
+      aggregateTimeout: 300
+    },
+    entry:        {
+      TemplatesTask: templatesTaskPath
+    },
+    devtool:      sourcemap,
+    output:       {
+      path:              path.resolve(__dirname, "dist/build"),
+      filename:          '[name]' + outputFileExt,
+      sourceMapFilename: '[name]' + sourceMapOutputFileExt,
+      libraryTarget:     'commonjs',
+      library:           '[name]'
+    },
+    resolve:      {
+      extensions: ['.js', '.ts']
+    },
+    target:       "node",
+    externals:    [],
+    module:       {
+      rules: [
+        {
+          test:    /\.ts/,
+          exclude: /node_modules\/(?!phusion|vue).*/,
+          use:     [
+            {
+              loader:  'ts-loader',
+              options: {
+                context: __dirname,
+              },
+            }
+          ]
+        }
+      ]
+    }
+  },
+  /**
+   * RoutesTask
+   */
+  {
+    mode:         mode,
+    watch:        false,
+    watchOptions: {
+      aggregateTimeout: 300
+    },
+    entry:        {
+      RoutesTask: routesTaskPath
+    },
+    devtool:      sourcemap,
+    output:       {
+      path:              path.resolve(__dirname, "dist/build"),
+      filename:          '[name]' + outputFileExt,
+      sourceMapFilename: '[name]' + sourceMapOutputFileExt,
+      libraryTarget:     'commonjs',
+      library:           '[name]'
+    },
+    resolve:      {
+      extensions: ['.js', '.ts']
+    },
+    target:       "node",
+    externals:    [],
     module:       {
       rules: [
         {
